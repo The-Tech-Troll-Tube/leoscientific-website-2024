7401d1b7c545a485192c4e6fa2a074960e0ba88d
The Tech Troll Tube <thetechtrolltube@gmail.com>
2024-09-07 18:22:22 +0530

    minor changes

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/albinjoseph/Documents/GitHub/leoscientific-website-2024/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/albinjoseph/Documents/GitHub/leoscientific-website-2024/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/albinjoseph/Documents/GitHub/leoscientific-website-2024/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/albinjoseph/Documents/GitHub/leoscientific-website-2024/src/css/custom.css"),
+];
